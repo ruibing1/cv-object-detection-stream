f4440dee9d8ae47c0793383967fa317746d79484
ruibing1 <ruibing187@proton.me>
2020-02-17 14:57:24 +0000

    Version 4.0.0

---
+++ OpenCV-yolo-stream-web.py
@@ -0,0 +1,13 @@
+
+# run in command prompt (no output files)
+# python OpenCV-yolo-stream-web.py --yolo yolo-coco --url https://youtu.be/1EiC9bvVGnk
+
+# run in command prompt (with output files)
+# python OpenCV-yolo-stream-web.py --yolo yolo-coco --url https://youtu.be/1EiC9bvVGnk --output output/ouput_videosteam.avi --data output/CSV/data_videosteam.csv 
+
+# JacksonHole streams https://youtu.be/RZWzyQuFxgE & https://youtu.be/1EiC9bvVGnk
+
+# import the necessary packages
+import numpy as np
+import pandas as pd
+import argparse
