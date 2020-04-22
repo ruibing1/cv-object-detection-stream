f2c99710b248b53cebdb4032b7d75b35252f026f
ruibing1 <ruibing187@proton.me>
2020-04-22 18:42:45 +0000

    [se] Fix

--- OpenCV-yolo-stream-web.py
+++ OpenCV-yolo-stream-web.py
@@ -57,3 +57,10 @@ COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
 # derive the paths to the YOLO weights and model configuration
 weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
 configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
+print("Initializing...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
