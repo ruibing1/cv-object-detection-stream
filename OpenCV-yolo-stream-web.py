4f94f814454300b591a4d1316967249d7dc93c94
ruibing1 <ruibing187@proton.me>
2020-06-07 05:36:33 +0000

    Update formats-data.js

--- OpenCV-yolo-stream-web.py
+++ OpenCV-yolo-stream-web.py
@@ -118,3 +118,14 @@ while True:
         for detection in output:
             # extract the class ID and confidence (i.e., probability)
             # of the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > args["confidence"]:
+                # scale the bounding box coordinates back relative to
+                # the size of the image, keeping in mind that YOLO
+                # actually returns the center (x, y)-coordinates of
+                # the bounding box followed by the boxes' width and
