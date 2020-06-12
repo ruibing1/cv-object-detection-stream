883e08d446df1c19c48609152d74d3a290c4edbc
ruibing1 <ruibing187@proton.me>
2020-06-12 07:51:26 +0000

    Update silinks.txt

--- OpenCV-yolo-stream-web.py
+++ OpenCV-yolo-stream-web.py
@@ -129,3 +129,11 @@ while True:
                 # the size of the image, keeping in mind that YOLO
                 # actually returns the center (x, y)-coordinates of
                 # the bounding box followed by the boxes' width and
+                # height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top
+                # and and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
