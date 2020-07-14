c91978f00551c7c912378f44f96adb3b6628b306
ruibing1 <ruibing187@proton.me>
2020-07-14 21:44:28 +0000

    staging: vt6656: 64 bit fixes: use u32 for QWORD definition.

--- OpenCV-yolo-stream.py
+++ OpenCV-yolo-stream.py
@@ -6,3 +6,17 @@
 # python OpenCV-yolo-stream.py --yolo yolo-coco --url https://youtu.be/1EiC9bvVGnk --output output/ouput_videosteam.avi --data output/CSV/data_videosteam.csv 
 
 # JacksonHole streams https://youtu.be/RZWzyQuFxgE & https://youtu.be/1EiC9bvVGnk
+
+# import the necessary packages
+import numpy as np
+import pandas as pd
+import argparse
+import time
+import datetime
+import cv2
+import os
+import pafy
+import streamlink
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
