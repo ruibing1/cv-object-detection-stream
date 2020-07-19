a5d00f5ef0c6b40b3e182a5f04cf23eb820f7e67
ruibing1 <ruibing187@proton.me>
2020-07-19 17:34:30 +0000

    merge branch

--- OpenCV-yolo-stream.py
+++ OpenCV-yolo-stream.py
@@ -49,3 +49,11 @@ COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
     dtype="uint8")
 
 # derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
+print("Initializing...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln = net.getLayerNames()
